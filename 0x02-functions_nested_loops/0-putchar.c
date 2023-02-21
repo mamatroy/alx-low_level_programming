5542fc1d7745732f6d58b1f30d1a0d5c290d3791
Polite <pmakah@gmail.com>
2023-02-21 14:19:54 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <"main.h">
+/**
+ * main - prints putchar
+ * followed by a new line
+ * return 0
+ */
+int main(void)
+{
+	_putchar ("_");
+	_putchar ("p");
+	_putchar ("u");
+	_putchar ("t");
+	_putchar ("c");
+	_putchar ("h");
+	_putchar ("a");
+	_putchar ("r"\n);
+Return: (0);
+}
