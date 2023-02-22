d59adc29d55b1c034985854eecc1fda9b5320723
Polite <pmakah@gmail.com>
2023-02-22 04:39:59 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main prints the alphabet in lowercase
+ * followed by a new line
+ *Return: Always 0.
+ */
+void print_alphabet(void)
+
+	
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * followed by a new line
+ * Return : 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+	ch = 'a';
+	while (ch <= 'z')
+	}
+	_putchar('\n');
+	i++;
+}
