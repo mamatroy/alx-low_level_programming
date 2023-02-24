452fd77fb27fa819efed64911aad023c1d773a21
Polite <pmakah@gmail.com>
2023-02-23 19:29:08 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,11 +23,11 @@ int main(void)
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-		printf("FizzBuzz")
+		printf("FizzBuzz");
 		}
 		else if (i == 1)
 		{
-		printf("%d" i);
+		printf("%d", i);
 		}
 		else
 		{
