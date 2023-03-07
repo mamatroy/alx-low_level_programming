4d16177fbe9571e8f3e8d9a26e1b8c3c8a0239a8
Polite <pmakah@gmail.com>
2023-03-07 12:21:12 -0800

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int n = 0;
 	int r;
 
-	while(*s)
+	while (*s)
 
 	{
 
@@ -25,7 +25,7 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 
-			else if (accept[r + 1] = '\0')
+			else if (accept[r + 1] - '\0')
 
 				return (n);
 		}
