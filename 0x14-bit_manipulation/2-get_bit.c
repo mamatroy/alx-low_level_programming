0aef693bde72b6a869507d03f6629a40855135c9
Polite <pmakah@gmail.com>
2023-04-09 07:54:22 -0700

    Two Point One Zero

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,9 +2,16 @@
 /**
  * get_bit-function that returns the value of a bit at a given index
  * @n:the unsigned integer
- * Return:the value of the bit at index index or 
+ * @index:index of bit to get
+ * Return:the value of the bit at index index or
  *        -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int value_of_bit;
 
+	if (index > 63)
+		return (-1);
+	value_of_bit = (n >> index) & 1;
+	return (value_of_bit);
+}
