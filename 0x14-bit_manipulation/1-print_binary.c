dacdde4e2fd3473d44de354f32ae0357e6fbef87
Polite <pmakah@gmail.com>
2023-04-10 09:52:23 -0700

    Ten

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,17 +8,18 @@ void print_binary(unsigned long int n)
 	int x, count = 0;
 	unsigned long int current;
 
-	for (x = 63; x >= 0; x++)
+	for (x = 63; x >= 0; x--)
 	{
 		current = n >> x;
+
 		if (current & 1)
 		{
-			_putchar ('1');
+			_putchar('1');
 			count++;
 		}
 		else if (count)
-			_putchar ('0');
+			_putchar('0');
 	}
 	if (!count)
-		_putchar ('0');
+		_putchar('0');
 }
